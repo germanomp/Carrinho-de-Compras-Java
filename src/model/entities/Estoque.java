9aaaa0ad03ca698d5160c398b3512e066b30e1d8
Germano Manzoni <germano12344@gmail.com>
2024-10-28 20:13:28 -0300

    classe estoque criada

---
+++ src/model/entities/Estoque.java
@@ -0,0 +1,86 @@
+package model.entities;
+
+import java.util.Objects;
+
+public class Estoque {
+
+    private Integer id;
+    private String nome;
+    private String categoria;
+    private Double valor;
+    private Integer quantidade;
+
+    public Estoque() {}
+
+    public Estoque(Integer id, String nome, String categoria, Double valor, Integer quantidade) {
+        this.id = id;
+        this.nome = nome;
+        this.categoria = categoria;
+        this.valor = valor;
+        this.quantidade = quantidade;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Estoque estoque = (Estoque) o;
+        return Objects.equals(id, estoque.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Estoque{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", categoria='" + categoria + '\'' +
+                ", valor=" + valor +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+}
